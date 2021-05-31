0d5caca6ccc4cd8cc07b17220b0fb06ed4ab47b8
wvvwcom <wvvwcom@qq.com>
2021-05-31 15:32:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,14 +11,14 @@
 # src-git kenzo https://github.com/kenzok8/openwrt-packages
 
 # fw876/helloworld
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
 #获取Lienol-xiaorouji-passwall
 # git clone https://github.com/xiaorouji/openwrt-package/lienol/ package/diy-packages/lienol
 # git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
-git clone https://github.com/kenzok8/openwrt-packages.git package/diy-packages
-git clone https://github.com/kenzok8/small.git package/small
+# git clone https://github.com/kenzok8/openwrt-packages.git package/diy-packages
+# git clone https://github.com/kenzok8/small.git package/small
 
 
 

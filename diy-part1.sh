c550f1c43381cd922f957a7511afdce4427847d5
wvvwcom <wvvwcom@qq.com>
2021-11-23 17:29:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -34,8 +34,11 @@ rm -rf package/helloworld
 git clone https://github.com/fw876/helloworld.git package/helloworld
 
 # project-lede/luci-app-godproxy
-rm -rf package/luci-app-godproxy
-git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
+# rm -rf package/luci-app-godproxy
+# git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
+# godros/luci-app-ikoolproxy
+rm -rf package/luci-app-ikoolproxy
+git clone https://github.com/godros/luci-app-ikoolproxy package/luci-app-ikoolproxy
 
 # package from lienol begin
 rm -rf package/lienol
